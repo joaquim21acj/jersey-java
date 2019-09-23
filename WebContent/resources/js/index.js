fa617aa2d8240c140ab4ac8016f3a138cd38a974
Joaquim Alves <joaquimjunior13@gmail.com>
2019-09-23 19:48:00 -0300

    Implementacao da edicao no front
    - bootstrap-vue.css adicionei algumas configuracoes para habilitar a
    edicao do produto direto na tabela
    - index.html modifiquei o formato da tabela para poder exibir os dados e
    ao clicar em editar ela exibir o campo com o valor anterior e ser
    possivel editar
    - index.js alguns scripts para auxiliar na edicao
    
    Falta implementar o botao de salvar

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,6 +1,7 @@
 var inicio = new Vue({
 	el:"#inicio",
     data: {
+    	editMode: false,
         listaProdutos: [],
         listaProdutosHeader: [
 			{sortable: false, key: "nome", label:"Nome"},
@@ -8,13 +9,21 @@ var inicio = new Vue({
 			{sortable: false, key: "volume", label:"Volume"},
 			{sortable: false, key: "unidade", label:"Unidade"},
 			{sortable: false, key: "estoque", label:"Estoque"}
-		]
+		],
+		produtoEditado: null
     },
     created: function(){
         let vm =  this;
         vm.buscaProdutos();
     },
     methods:{
+    	salvar () {
+    	      
+        },
+        editar (produto) {
+            this.beforEditCache = produto
+            this.produtoEditado = produto
+          },
         buscaProdutos: function(){
 			const vm = this;
 			axios.get("/mercado/rs/produtos")
@@ -26,7 +35,28 @@ var inicio = new Vue({
 		},
     }
 });
-function ApagarProduto(id){
-	
-	
-}
+//function editaProduto(){
+//	alert("aqui");
+//}
+
+function editaProduto(produto){
+	alert("Aqui");
+	alert(nome);
+    var par = $(this).parent().parent(); //tr
+    var nome = par.children("td:nth-child(1)");
+    var nomeFabricante = par.children("td:nth-child(2)");
+    var volume = par.children("td:nth-child(3)");
+    var unidade = par.children("td:nth-child(4)");
+    var estoque = par.children("td:nth-child(5)");
+    var salvar = par.children("td:nth-child(6)");
+ 
+    nome.html("<input type='text' id='txtName' value='"+nome.html()+"'/>");
+    nomeFabricante.html("<input type='text' id='txtPhone' value='"+nomeFabricante.html()+"'/>");
+    volume.html("<input type='text' id='txtEmail' value='"+volume.html()+"'/>");
+    unidade.html("<input type='text' id='txtEmail' value='"+unidade.html()+"'/>");
+    estoque.html("<input type='text' id='txtEmail' value='"+estoque.html()+"'/>");
+    salvar.html("<button>ok</button>");
+ 
+    $(".btnSave").bind("click", Save);
+
+};
